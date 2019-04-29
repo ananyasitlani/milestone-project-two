11b68ea69d6a2983ddca9f82c1af18f1c51df53e
Ananya Sitlani <ananyasitlani@gmail.com>
2019-04-29 16:05:57 +0000

    add mouseenter and mouseleave to countries

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,55 @@
-/*--------------------------------------------Countries to Castles*/
+// Hover on Country
 
 $(document).ready(function() {
+    $(".england").mouseenter(function(){
+        $('.england').fadeTo(1000, 0.5);
+    $(".england").mouseleave(function(){
+        $('.england').fadeTo(1000, 1);
+    });
+    });
+    $(".scotland").mouseenter(function(){
+        $('.scotland').fadeTo(1000, 0.5);
+    $(".scotland").mouseleave(function(){
+        $('.scotland').fadeTo(1000, 1);
+    }); 
+    });
+    $(".wales").mouseenter(function(){
+        $('.wales').fadeTo(1000, 0.5);
+    $(".wales").mouseleave(function(){
+        $('.wales').fadeTo(1000, 1);
+    });
+    });
+    $(".northernireland").mouseenter(function(){
+        $('.northernireland').fadeTo(1000, 0.5);
+    $(".northernireland").mouseleave(function(){
+        $('.northernireland').fadeTo(1000, 1);
+    });
+    });
+// Countries to Castles 
+
     $(".england").click(function(){
-            $('.england-castles').show('slow');
-            $('.scotland-castles').hide('slow');
-            $('.wales-castles').hide('slow');
-            $('.northernireland-castles').hide('slow');
+            $('.england-castles').slideToggle('1000');
+            $('.scotland-castles').slideUp('1000');
+            $('.wales-castles').slideUp('1000');
+            $('.northernireland-castles').slideUp('1000');
         });
     $(".scotland").click(function(){
-            $('.england-castles').hide('slow');
-            $('.scotland-castles').show('slow');
-            $('.wales-castles').hide('slow');
-            $('.northernireland-castles').hide('slow');
+            $('.england-castles').slideUp('1000');
+            $('.scotland-castles').slideToggle('1000');
+            $('.wales-castles').slideUp('1000');
+            $('.northernireland-castles').slideUp('1000');
         });
     $(".wales").click(function(){
-            $('.england-castles').hide('slow');
-            $('.scotland-castles').hide('slow');
-            $('.wales-castles').show('slow');
-            $('.northernireland-castles').hide('slow');
+            $('.england-castles').slideUp('1000');
+            $('.scotland-castles').slideUp('1000');
+            $('.wales-castles').slideToggle('1000');
+            $('.northernireland-castles').slideUp('1000');
         });
     $(".northernireland").click(function(){
-            $('.england-castles').hide('slow');
-            $('.scotland-castles').hide('slow');
-            $('.wales-castles').hide('slow');
-            $('.northernireland-castles').show('slow');
+            $('.england-castles').slideUp('1000');
+            $('.scotland-castles').slideUp('1000');
+            $('.wales-castles').slideUp('1000');('slow');
+            $('.northernireland-castles').slideToggle('1000');
         });
 });
+
