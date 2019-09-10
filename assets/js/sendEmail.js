acead001ce59c1debde1da3e307da8211a0e7847
Ubuntu <ubuntu@ip-172-31-83-44.ec2.internal>
2019-09-10 15:26:16 +0000

    form js added in

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,14 @@
+function sendMail (contactForm) {
+    emailjs.send("gmail", "milestone_project_two" {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "location": contactForm.location.value
+    })
+    .then(
+        function(response) {
+            console.log("REQUEST SENT!", response);
+        }, 
+        function(error){
+            console.log("FAILED", error)
+        })
+}
