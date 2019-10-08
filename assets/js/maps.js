1bef179c18d180d1b69b295a757ff5e9ffa86e46
Ubuntu <ubuntu@ip-172-31-83-44.ec2.internal>
2019-10-08 17:08:34 +0000

    castle markers are now on the map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -12,162 +12,63 @@ function initMap() {
     
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
  
-    // // array of castles for markers
-    // var castles = [{
-    //     'windsorcastle': {
-    //         longitude: 51.483860,
-    //         latitude: -0.606490
-    //     },
-    //     'leedscastle': {
-    //         longitude: 51.248844,
-    //         latitude: 0.630249
-    //     },
-    //     'dovercastle': {
-    //         longitude: 51.129524,
-    //         latitude: 1.321235
-    //     },
-    //     'warwickcastle': {
-    //         longitude: 52.279472,
-    //         latitude: -1.585041
-    //     },
-    //     'bodiamcastle': {
-    //         longitude: 51.001728,
-    //         latitude: 0.543282
-    //     },
-    //     'lincolncastle': {
-    //         longitude: 53.234745,
-    //         latitude: -0.540397
-    //     },
-    //     'durhamcastle': {
-    //         longitude: 54.77546,
-    //         latitude: -1.576635
-    //     },
-    //     'ludlowcastle': {
-    //         longitude: 52.36722,
-    //         latitude: -2.722394
-    //     },
-    //     'edinburghcastle': {
-    //         longitude: 55.948814,
-    //         latitude: -3.200283
-    //     },
-    //     'braemarcastle': {
-    //         longitude: 57.014683,
-    //         latitude: -3.391364
-    //     },
-    //     'balmoralcastle': {
-    //         longitude: 57.04142,
-    //         latitude: -3.228998
-    //     },
-    //     'blaircastle': {
-    //         longitude: 56.77357,
-    //         latitude: -3.857812
-    //     },
-    //     'inveraraycastle': {
-    //         longitude: 56.23773,
-    //         latitude: -5.073967
-    //     },
-    //     'cawdorcastle': {
-    //         longitude: 57.524105,
-    //         latitude: -3.925715
-    //     },
-    //     'eileandonancastle': {
-    //         longitude: 57.274677,
-    //         latitude: -5.513736
-    //     },
-    //     'dunnottarcastle': {
-    //         longitude: 56.945793,
-    //         latitude: -2.200614
-    //     },
-    //     'conwycastle': {
-    //         longitude: 53.280052,
-    //         latitude: -3.825447
-    //     },
-    //     'pembrokecastle': {
-    //         longitude: 51.676678,
-    //         latitude: -4.91993
-    //     },
-    //     'carregcennencastle': {
-    //         longitude: 51.856617,
-    //         latitude: -3.93721
-    //     },
-    //     'harlechcastle': {
-    //         longitude: 52.859726,
-    //         latitude: -4.108152
-    //     },
-    //     'caerphillycastle': {
-    //         longitude: 51.57615,
-    //         latitude: -3.220367
-    //     },
-    //     'penrhyncastle': {
-    //         longitude: 53.225117,
-    //         latitude: -3.220367
-    //     },
-    //     'cardiffcastle': {
-    //         longitude: 51.48149,
-    //         latitude: -3.180798
-    //     },
-    //     'chepstowcastle': {
-    //         longitude: 51.64453,
-    //         latitude: -2.673271
-    //     },
-    //     'belfastcastle': {
-    //         longitude: 54.64268,
-    //         latitude: -5.942325
-    //     },
-    //     'carrickferguscastle': {
-    //         longitude: 54.713737,
-    //         latitude: -5.806532
-    //     },
-    //     'glenarmcastle': {
-    //         longitude: 54.96765,
-    //         latitude: -5.959837
-    //     },
-    //     'dunlucecastle': {
-    //         longitude: 55.212536,
-    //         latitude: -6.575105
-    //     },
-    //     'cromcastle': {
-    //         longitude: 54.182605,
-    //         latitude: -7.36255
-    //     },
-    //     'enniskillencastle': {
-    //         longitude: 54.34579,
-    //         latitude: -7.643679
-    //     },
-    //     'hillsboroughcastle': {
-    //         longitude: 54.46123,
-    //         latitude: -6.085229
-    //     },
-    //     'hillsboroughcastle': {
-    //         longitude: 54.4608,
-    //         latitude: -6.3294
-    //     },
-        
-    // }];
+    // array of castles for markers
     
     var castles = [
-        ['windsorcastle', 51.483860, -0.606490],
-        ['leedscastle', 51.248844, -0.630249],
-        ['dovercastle', 51.129524, -1.321235],
-    ]
+            ['windsorcastle', 51.483860, -0.606490],
+            ['leedscastle', 51.248844, -0.630249],
+            ['dovercastle', 51.129524, -1.321235],
+            ['warwickcastle', 52.279472, -1.585041],
+            ['bodiamcastle', 51.001728, 0.543282],
+            ['lincolncastle', 53.234745, -0.540397],
+            ['durhamcastle', 54.77546, -1.576635],
+            ['ludlowcastle', 52.36722, -2.722394],
+            ['edinburghcastle', 55.948814, -3.200283],
+            ['braemarcastle', 57.014683, -3.391364],
+            ['balmoralcastle', 57.04142, -3.228998],
+            ['blaircastle', 56.77357, -3.857812],
+            ['inveraraycastle', 56.23773, -5.073967],
+            ['cawdorcastle', 57.524105, -3.925715],
+            ['eileandonancastle', 57.274677, -5.513736],
+            ['dunnottarcastle', 56.945793, -2.200614],
+            ['conwycastle', 53.280052, -3.825447],
+            ['pembrokecastle', 51.676678, -4.91993],
+            ['carregcennencastle', 51.856617, -3.93721],
+            ['harlechcastle', 52.859726, -4.108152],
+            ['caerphillycastle', 51.57615, -3.220367],
+            ['penrhyncastle', 53.225117, -3.220367],
+            ['cardiffcastle', 51.48149, -3.180798],
+            ['chepstowcastle', 51.64453, -2.673271],
+            ['belfastcastle', 54.64268, -5.942325],
+            ['carrickferguscastle', 54.713737, -5.806532],
+            ['glenarmcastle', 54.96765, -5.959837],
+            ['dunlucecastle', 55.212536, -6.575105],
+            ['cromcastle', 54.182605, -7.36255],
+            ['enniskillencastle', 54.34579, -7.643679],
+            ['hillsboroughcastle', 54.46123, -6.085229],
+            ['lurgancastle', 54.4608, -6.3294]
+            
+        ]
         
-        
+    
     // loop through markers
     for( i = 0; i < castles.length; i++ ){
-        var addMarker = ("The lat is " + castles[i][1]+" and the long is "+ castles[i][2]);
         var marker = new google.maps.Marker({
-        position: {lat: castles[1], lng: castles[2]},
+        position: {lat: castles[i][1], lng: castles[i][2]},
         map: map
     });
-}
+} 
+
+    document.getElementByClassName("castle").addEventListener("click", marker);
+    
   
   
-    // // function to show markers on map
-    // function addMarker(marks){
-    //     var marker = new google.maps.Marker({
-    //     position: marks.castles,
-    //     map: map
-    // });
-    // }
+//     // // function to show markers on map
+//     // function addMarker(marks){
+//     //     var marker = new google.maps.Marker({
+//     //     position: marks.castles,
+//     //     map: map
+//     // });
+//     // }
     
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,6 @@
 // Hover on Country
 
 $(document).ready(function() {
-    $(".england").mouseenter(function(){
-        $('.england').fadeTo(1000, 0.5);
-    $(".england").mouseleave(function(){
-        $('.england').fadeTo(1000, 1);
-    });
-    });
-    $(".scotland").mouseenter(function(){
-        $('.scotland').fadeTo(1000, 0.5);
-    $(".scotland").mouseleave(function(){
-        $('.scotland').fadeTo(1000, 1);
-    }); 
-    });
-    $(".wales").mouseenter(function(){
-        $('.wales').fadeTo(1000, 0.5);
-    $(".wales").mouseleave(function(){
-        $('.wales').fadeTo(1000, 1);
-    });
-    });
-    $(".northernireland").mouseenter(function(){
-        $('.northernireland').fadeTo(1000, 0.5);
-    $(".northernireland").mouseleave(function(){
-        $('.northernireland').fadeTo(1000, 1);
-    });
-    });
 
 // Countries to Castles 
 
@@ -58,8 +34,6 @@ $(document).ready(function() {
 $ (".castle-styles").click(function(){
        $(this).toggleClass("chosen"); 
     });
-    
-
     
     // reset button
     
